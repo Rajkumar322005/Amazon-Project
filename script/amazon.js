e8ad508210fc2d8c37ad2397e078a1dd8422f598
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-05-23 20:36:55 +0530

    Used Module

--- script/amazon.js
+++ script/amazon.js
@@ -1,23 +1,6 @@
-// const products = [
-//   {
-//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-//     rating: {
-//       stars: 4.5,
-//       count: 87,
-//     },
-//     price: 1070,
-//   },
-//   {
-//     image: "images/products/intermediate-composite-basketball.jpg",
-//     name: "Intermediate Size Basketball",
-//     rating:{
-//         stars:4.0,
-//         count:127
-//     },
-//     price:2095,
-//   },
-// ];
+
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 
 let combinedHtml = '';
 products.forEach((products)=>{
