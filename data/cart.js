8ee993864206b1e07effa742284a95750abf6355
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-05-26 01:20:40 +0530

    local storage updated

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')); 
+
+if(!cart){
+  cart = [
     {
-        productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
     },
     {
-        productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-        quantity : 2
-    }
-];
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 2,
+    },
+  ];
+};
+function saveTostorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+saveTostorage();
 export function addTocart(productid){
   let matchItem;
   //product name may be same
@@ -25,6 +33,7 @@ export function addTocart(productid){
       quantity: 1,
     });
   }
+  saveTostorage();
 }
 
 export function removeCart(productid){
@@ -35,4 +44,5 @@ export function removeCart(productid){
     };
   });
   cart = newCart;
+  saveTostorage();
 };
