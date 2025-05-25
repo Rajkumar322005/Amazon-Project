3ca699da97d3d10e124fcb40068d2bbcf58e42ac
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-05-26 00:40:12 +0530

    delete products using remove() function

--- script/checkout.js
+++ script/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) =>{
     });
     //console.log(matchItem);
     cartSummary += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchItem.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -39,7 +39,9 @@ cart.forEach((cartItem) =>{
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchItem.id}">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${
+                  matchItem.id
+                }">
                 Delete
                 </span>
             </div>
@@ -100,6 +102,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener('click',()=>{
         const productid = link.dataset.productId;
         removeCart(productid);
-        console.log(cart);
-    })
-})
+        
+        const container = document.querySelector(`.js-cart-item-container-${productid}`);
+        container.remove();
+    });
+});
