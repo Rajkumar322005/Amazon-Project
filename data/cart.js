96909c5e58d5b362cfa82d59c4ad48824c6df3ec
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-05-30 23:46:17 +0530

    product updated

--- data/cart.js
+++ data/cart.js
@@ -3,12 +3,12 @@ export let cart = JSON.parse(localStorage.getItem("cart"));
 if (!cart) {
   cart = [
     {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity:1,
       deliveryOptionsId:"1", 
     },
     {
-      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      productId:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
       quantity: 2,
       deliveryOptionsId: "3", 
     },
