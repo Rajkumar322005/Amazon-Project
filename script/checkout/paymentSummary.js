dcfe8be8f1c3ed78e447c7b866847b64230dd2ac
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-01 23:33:40 +0530

    Generate Html and save code for payment Summary

--- script/checkout/paymentSummary.js
+++ script/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getdeliveryOption } from '../../data/delivaryoptions.js';
+import { format_money } from '../utils/money.js';
 export function renderPaymentSummary(){
     let cost = 0;
     let shippingcost = 0;
@@ -15,9 +16,46 @@ export function renderPaymentSummary(){
     });
     //console.log(cost);
     //console.log(shippingcost);
-    // const beforeTax = cost + shippingcost;
-    // const totaltax = beforeTax*0.1;
-    // const totalCents = beforeTax + totaltax;
+    const beforeTax = cost + shippingcost;
+    const totaltax = beforeTax*0.1;
+    const totalCents = beforeTax + totaltax;
 
-    // const 
+    const paymentSummaryHtml = 
+    `
+        <div class="payment-summary-title">
+            Order Summary
+        </div>
+
+        <div class="payment-summary-row">
+        <div>Items (3):</div>
+
+        <div class="payment-summary-money">$${format_money(cost)}</div>
+        </div>
+
+        <div class="payment-summary-row">
+        <div>Shipping &amp; handling:</div>
+        <div class="payment-summary-money">$${format_money(shippingcost)}</div>
+        </div>
+
+        <div class="payment-summary-row subtotal-row">
+        <div>Total before tax:</div>
+        <div class="payment-summary-money">$${format_money(beforeTax)}</div>
+        </div>
+
+        <div class="payment-summary-row">
+        <div>Estimated tax (10%):</div>
+        <div class="payment-summary-money">$${format_money(totaltax)}</div>
+        </div>
+
+        <div class="payment-summary-row total-row">
+        <div>Order total:</div>
+        <div class="payment-summary-money">$${format_money(totalCents)}</div>
+        </div>
+
+        <button class="place-order-button button-primary">
+        Place your order
+        </button>
+    `
+
+    document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHtml;
 };
